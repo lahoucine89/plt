b9827e418f981f3c52da8362cf43869f739b3b28
lahoucine89 <107364894+lahoucine89@users.noreply.github.com>
2024-10-31 18:19:10 +0200

    Update test_piglatin.py

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -2,8 +2,29 @@ import unittest
 from piglatin import PigLatin
 from error import PigLatinError
 
+class TestPigLatin(unittest.TestCase):
+    def test_get_phrase(self):
+        translator = PigLatin("hello world")
+        self.assertEqual(translator.get_phrase(), "hello world")
 
 class TestPigLatin(unittest.TestCase):
+    def test_get_phrase(self):
+        translator = PigLatin("hello world")
+        self.assertEqual(translator.get_phrase(), "hello world")
+    def test_translate_empty_phrase(self):
+        translator = PigLatin("")
+        self.assertEqual(translator.translate(), "nil")
+    
+class TestPigLatin(unittest.TestCase):
+   
+    def test_translate_vowel_start_word(self):
+        translator = PigLatin("any")
+        self.assertEqual(translator.translate(), "anynay")
 
-    def test_something(self):
-        pass
+        translator = PigLatin("apple")
+        self.assertEqual(translator.translate(), "appleyay")
+
+       
+        translator = PigLatin("ask")
+        self.assertEqual(translator.translate(), "askay")
+    
